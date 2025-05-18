33df9ed2e8367e8c4fb0c16a852ff419e46f4a0d
Jitesh Yadav <jiteshyadav1990@gmail.com>
2025-05-18 16:31:50 -0400

    fix: lint and test fix

--- plugins/synergy-backend/src/service/router.test.ts
+++ plugins/synergy-backend/src/service/router.test.ts
@@ -14,6 +14,7 @@ describe('createRouter', () => {
         data: {
           synergy: {
             provider: {
+              type: 'github',
               github: {
                 org: 'test',
                 host: 'test',
